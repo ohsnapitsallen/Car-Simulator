56d34cb76b402d4497a44b553298dd0691f855ab
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 15:18:54 +0800

    Added class

--- CarTest.py
+++ CarTest.py
@@ -2,6 +2,7 @@
 from Car import *
 
 #Create a class for testing
+class CarTest:
 #Create a method for testing
 #Create an object with the car's year and make
 #Call the accelerate method using for loop
