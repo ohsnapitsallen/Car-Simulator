337e1502144944b1fa2e144c4f46950f68477c56
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 14:36:22 +0800

    Pseudocodes

---
+++ Car.py
@@ -0,0 +1,5 @@
+#Create Car class
+#Create class constructor for the car's year make
+#Create a class for car's accelerating speed value
+#Create a class for car's braking speed value
+#Create a getter method for the car speed
