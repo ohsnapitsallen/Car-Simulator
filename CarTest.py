bc997a052b0e369c403b0d7db687ad7df6e464ac
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 18:51:51 +0800

    Added car object with year and make

--- CarTest.py
+++ CarTest.py
@@ -6,6 +6,8 @@ class CarTest:
 #Create a method for testing
     def main():
 #Create an object with the car's year and make
+        my_car = Car(2020, "Chevrolet")
+        
 #Call the accelerate method using for loop
 #Call the brake method using for loop           
 #Start the test program
