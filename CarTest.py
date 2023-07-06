c37854384e9c08fd28b4dfa9f588dd9e31c3fd01
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 15:20:58 +0800

    Added main function

--- CarTest.py
+++ CarTest.py
@@ -4,6 +4,7 @@ from Car import *
 #Create a class for testing
 class CarTest:
 #Create a method for testing
+    def main():
 #Create an object with the car's year and make
 #Call the accelerate method using for loop
 #Call the brake method using for loop           
