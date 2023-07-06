7ecb5982071d351cb2fa6362023b47824228c484
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 14:57:29 +0800

    Added constructor

--- Car.py
+++ Car.py
@@ -1,5 +1,10 @@
 #Create Car class
-class Car:#Create class constructor for the car's year make
+class Car:
+#Create class constructor for the car's year make
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
 #Create a class for car's accelerating speed value
 #Create a class for car's braking speed value
 #Create a getter method for the car speed
