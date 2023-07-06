c4446acbaef74e0f790563b6bb0b3df83f3e6875
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 19:09:31 +0800

    Brake Method Added

--- CarTest.py
+++ CarTest.py
@@ -12,5 +12,10 @@ class CarTest:
         for i in range(5):
             my_car.accelerate()
             print("Current speed after accelerating:", my_car.get_speed())
-#Call the brake method using for loop           
+
+#Call the brake method using for loop
+        for i in range(5):
+            my_car.brake()
+            print("Current speed after braking:", my_car.get_speed())
+                  
 #Start the test program
